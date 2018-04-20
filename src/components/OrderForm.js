c25b13791052de8d3e90ec486d912c0352835f7a
Alexander Urizar <urizaraj@gmail.com>
2018-04-20 11:50:15 -0400

    Add ItemList, Details, and ChevButton components

--- src/components/OrderForm.js
+++ src/components/OrderForm.js
@@ -9,21 +9,55 @@ const Row = props => (
   </div>
 )
 
-const Button = props => (
-  <button className="btn btn-primary" onClick={() => props.handleClick(props.id)} >
-    <Icon icon='plus' />
-  </button>
-)
-
-const Item = props => (
-  <div>
-    {props.name} <Button handleClick={props.handleClick} id={props.id} />
-  </div>
-)
-
 const Col = props => (
   <div className='col' >
     {props.children}
+  </div>
+)
+
+const Button = props => (
+  <button className="btn btn-primary" onClick={() => props.handleClick(props.id)} >
+    <Icon icon='plus' /> Add to Order
+  </button>
+)
+
+// const Item = props => (
+//   <div>
+//     {props.name} <Button handleClick={props.handleClick} id={props.id} />
+//   </div>
+// )
+
+const ChevButton = ({onClick}) => (
+  <button className="btn btn-primary" {...{onClick}} ><Icon icon='chevron-down' /></button>
+)
+
+class Item extends Component {
+  constructor() {
+    super()
+    this.state = {
+      active: false
+    }
+  }
+
+  render() {
+    return (
+      <div className='mb-3' >
+        <ChevButton onClick={this.toggleActive.bind(this)} /> {this.props.name}
+        {this.state.active && <Details handleClick={this.props.handleClick} id={this.props.id} />}
+      </div>
+    )
+  }
+
+  toggleActive() {
+    this.setState({
+      active: !this.state.active
+    })
+  }
+}
+
+const Details = props => (
+  <div>
+    <Button handleClick={props.handleClick} id={props.id} />
   </div>
 )
 
@@ -31,9 +65,10 @@ const CurrentOrder = props => {
   const entries = Object.entries(props.order)
   let total = 0
   const items = entries.map(entry => {
-    const [id, count] = entry
+    const id = parseInt(entry[0])
+    const count = entry[1]
 
-    const item = props.items.find(i => i.id === parseInt(id))
+    const item = props.items.find(i => i.id === id)
 
     total += item.price * count
 
@@ -48,11 +83,18 @@ const CurrentOrder = props => {
     <div>
       <h4>Current Order</h4>
       {items}
-      <hr/>
+      <hr />
       <h4>Total</h4>
       ${total}
     </div>
   )
+}
+
+const ItemList = ({ items, handleClick }) => {
+  return items.map(item => {
+    const options = { ...item, handleClick }
+    return <Item {...options} key={item.id} />
+  })
 }
 
 class OrderForm extends Component {
@@ -68,11 +110,16 @@ class OrderForm extends Component {
     const items = []
     this.props.categories.forEach(category => category.items.forEach(item => items.push(item)))
 
+    const itemListProps = {
+      items,
+      handleClick: this.handleClick.bind(this)
+    }
+
     return (
       <div>
         <Row>
           <Col>
-            {items.map(item => <Item {...item} handleClick={this.handleClick.bind(this)} key={item.id} />)}
+            <ItemList {...itemListProps} />
           </Col>
           <Col>
             <CurrentOrder items={items} order={this.state.order} />
