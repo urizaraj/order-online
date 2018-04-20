f18f17acf777d180fdbb3548444fe09fa17a13c0
Alexander Urizar <urizaraj@gmail.com>
2018-04-20 16:40:05 -0400

    create activeItemReducer

---
+++ src/reducers/activeItemReducer.js
@@ -0,0 +1,9 @@
+const activeItemReducer = (state = {activeItem: null, selectedOptions: []}, action) => {
+  switch(action.type) {
+    case 'ADD_ACTIVE_ITEM':
+      return state
+
+    default: 
+      return state
+  }
+}
