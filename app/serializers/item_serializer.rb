83e85204cb5eb9b7be0c43d5a619068c0c3f15ac
Alexander Urizar <urizaraj@gmail.com>
2018-04-20 15:26:32 -0400

    Serialize options

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,3 +1,4 @@
 class ItemSerializer < ActiveModel::Serializer
   attributes :id, :name, :description, :price
+  has_many :options
 end
--- app/serializers/option_serializer.rb
+++ app/serializers/option_serializer.rb
@@ -1,3 +1,3 @@
 class OptionSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :price
 end
