167683e70451116a249555544312c00f6a0884b1
Alexander Urizar <urizaraj@gmail.com>
2018-04-20 10:45:23 -0400

    Add fontawesome icons react package

--- src/index.js
+++ src/index.js
@@ -8,8 +8,13 @@ import thunk from 'redux-thunk';
 import rootReducer from './reducers'
 
 import 'bootstrap/dist/css/bootstrap.min.css'
-// import './index.css';
+
+import fontawesome from '@fortawesome/fontawesome'
+import FontAwesomeIcon from '@fortawesome/react-fontawesome'
+import solid from '@fortawesome/fontawesome-free-solid'
 import App from './components/App';
+
+fontawesome.library.add(solid)
 
 const e = React.createElement
 const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk))
