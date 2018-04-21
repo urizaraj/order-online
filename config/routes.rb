996dfc8ae93eaff8dcc613e3d2b7a661d4f08804
Alexander Urizar <urizaraj@gmail.com>
2018-04-21 16:15:11 -0400

    Formatting

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
-  resources :selected_options
-  resources :order_items
-  resources :orders
-  resources :options
   resources :locations
-  resources :items
-  resources :categories
   resources :menus
+  resources :categories
+  resources :items
+  resources :options
+  
+  resources :orders
+  resources :order_items
+  resources :selected_options
 end
