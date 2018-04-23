ade61b23541cbfb54bd6774f55873b81fc24b737
Alexander Urizar <urizaraj@gmail.com>
2018-04-23 15:43:39 -0400

    Create user resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_21_200602) do
+ActiveRecord::Schema.define(version: 2018_04_23_194005) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -67,6 +67,15 @@ ActiveRecord::Schema.define(version: 2018_04_21_200602) do
   create_table "selected_options", force: :cascade do |t|
     t.integer "order_item_id"
     t.integer "option_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.boolean "admin"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
