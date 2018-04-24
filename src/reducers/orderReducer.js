4ee012e55eff67e4a46f3295a42a8ec02162dd4f
Alexander Urizar <urizaraj@gmail.com>
2018-04-24 15:18:39 -0400

    formatting

--- src/reducers/orderReducer.js
+++ src/reducers/orderReducer.js
@@ -1,13 +1,13 @@
 const orderReducer = (state = { items: [] }, action) => {
   switch (action.type) {
     case 'ADD_ORDER_ITEM':
-      return { items: [...state.items, action.item]}
+      return { items: [...state.items, action.item] }
 
     case 'REMOVE_ORDER_ITEM':
-      return { items: state.items.filter(item => item.cuid !== action.cuid)}
+      return { items: state.items.filter(item => item.cuid !== action.cuid) }
 
     case 'RESET_ORDER':
-      return {items: []}
+      return { items: [] }
 
     default:
       return state
