08719bb8cb624a5e200edf91c4a2d4a5aa9bc944
Alexander Urizar <urizaraj@gmail.com>
2018-04-24 13:21:34 -0400

    Users Controller formatting

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,12 @@
 class UsersController < ApplicationController
   def sign_in
     user = User.find_by(name: params[:name])
-    
+
     valid = user && user.authenticate(params[:password])
-
     return render json: {message: 'invalid'} unless valid
-    
+
     payload = {user: user.id}
-
     token = JWT.encode payload, nil, 'none'
-
     render json: {token: token, id: user.id, name: user.name}
   end
 
