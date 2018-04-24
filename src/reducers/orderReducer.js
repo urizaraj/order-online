c04c0e19e458d9816adfc8d1e5bc17064abe2e7c
Alexander Urizar <urizaraj@gmail.com>
2018-04-24 17:02:06 -0400

    orderReducer adding and removing order items does not affect other items in state

--- src/reducers/orderReducer.js
+++ src/reducers/orderReducer.js
@@ -1,10 +1,10 @@
 const orderReducer = (state = { items: [], id: null, index: [], loading: false }, action) => {
   switch (action.type) {
     case 'ADD_ORDER_ITEM':
-      return { items: [...state.items, action.item] }
+      return { ...state, items: [...state.items, action.item] }
 
     case 'REMOVE_ORDER_ITEM':
-      return { items: state.items.filter(item => item.cuid !== action.cuid) }
+      return { ...state, items: state.items.filter(item => item.cuid !== action.cuid) }
 
     case 'RESET_ORDER':
       return { ...state, items: [], id: null }
