3b89c4e8c26980dab585cdbdb898ae311d1b50f8
Alexander Urizar <urizaraj@gmail.com>
2018-04-25 11:58:21 -0400

    Adjust userActions to store user information locally

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -14,34 +14,53 @@ export function userSignIn(user) {
         if (resp.message) return dispatch({ type: 'INVALID_SIGN_IN' })
         const { token, ...user } = resp
         localStorage.setItem('token', token)
+        localStorage.setItem('userId', user.id)
+        localStorage.setItem('userName', user.name)
         dispatch({ type: 'SIGNED_IN', ...user })
       })
   }
 }
 
+// export function userCheckToken() {
+//   return dispatch => {
+//     const token = localStorage.getItem('token')
+//     if (!token) return
+
+//     const options = {
+//       method: 'POST',
+//       body: JSON.stringify({ token }),
+//       headers: {
+//         'Content-Type': 'application/json'
+//       }
+//     }
+
+//     return fetch('/users/sign_in_token', options)
+//       .then(resp => resp.json())
+//       .then(resp => {
+//         if (resp.message) return dispatch({ type: 'INVALID_SIGN_IN' })
+//         dispatch({ type: 'SIGNED_IN', ...resp })
+//       })
+//   }
+// }
+
 export function userCheckToken() {
-  return dispatch => {
-    const token = localStorage.getItem('token')
-    if (!token) return
-
-    const options = {
-      method: 'POST',
-      body: JSON.stringify({ token }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
+  const token = localStorage.getItem('token')
+  if (token) {
+    return {
+      type: 'SIGNED_IN',
+      id: localStorage.getItem('userId'),
+      name: localStorage.getItem('userName')
     }
-
-    return fetch('/users/sign_in_token', options)
-      .then(resp => resp.json())
-      .then(resp => {
-        if (resp.message) return dispatch({ type: 'INVALID_SIGN_IN' })
-        dispatch({ type: 'SIGNED_IN', ...resp })
-      })
+  } else {
+    return {
+      type: 'NOT_SIGNED_IN'
+    }
   }
 }
 
 export function userSignOut() {
-  localStorage.removeItem('token')
+  for (let item of ['token', 'userId', 'userName']) {
+    localStorage.removeItem(item)
+  }
   return { type: 'SIGN_OUT' }
 }
