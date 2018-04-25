cbd8439fd8144cf359729e52c003bbaeac5357fe
Alexander Urizar <urizaraj@gmail.com>
2018-04-25 16:32:01 -0400

    Route to new location path

--- src/components/LocationsPage.js
+++ src/components/LocationsPage.js
@@ -3,11 +3,13 @@ import { Route, Switch } from 'react-router-dom'
 
 import LocationsIndex from './LocationsIndex'
 import LocationsShow from './LocationsShow'
+import LocationsNew from './LocationNew'
 
 const LocationsPage = props => {
   return (
     <div>
       <Switch>
+        <Route path={`${props.match.url}/new`} component={LocationsNew} />
         <Route path={`${props.match.url}/:locationId`} component={LocationsShow} />
         <Route path={`${props.match.url}`} component={LocationsIndex} />
       </Switch>
