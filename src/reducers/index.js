e5dcfcfae373f2248c0f98d7ad3965d8807ae56b
Alexander Urizar <urizaraj@gmail.com>
2018-04-25 16:31:25 -0400

    Add newLocationReducer to rootReducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,13 +4,15 @@ import activeItemReducer from './activeItemReducer'
 import orderReducer from './orderReducer'
 import locationReducer from './locationReducer'
 import userReducer from './userReducer'
+import newLocationReducer from './newLocationReducer'
 
 const rootReducer = combineReducers({
   menu: menuReducer,
   activeItem: activeItemReducer,
   order: orderReducer,
   locations: locationReducer,
-  user: userReducer
+  user: userReducer,
+  newLocation: newLocationReducer
 })
 
 export default rootReducer
