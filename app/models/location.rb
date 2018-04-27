058267b109a6cc791ee28317d8ca1b02b638fd20
Alexander Urizar <urizaraj@gmail.com>
2018-04-27 16:27:33 -0400

    Configure has many through associations for Location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,5 +1,8 @@
 class Location < ApplicationRecord
   has_many :menus
+  has_many :categories, through: :menus
+  has_many :items, through: :categories
+  has_many :options, through: :items
 
   accepts_nested_attributes_for :menus
 end
