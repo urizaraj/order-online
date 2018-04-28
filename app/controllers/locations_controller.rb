dc5861a6f32417893377a3edde6ddf9ac3fdaa2c
Alexander Urizar <urizaraj@gmail.com>
2018-04-28 15:09:58 -0400

    Locations controller handles update requests

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class LocationsController < ApplicationController
   def index
     render json: Location.all
   end
 
-  def show 
+  def show
     location = Location.find(params[:id])
 
     return render json: location, serializer: LocationEditSerializer if params[:edit]
@@ -13,7 +15,13 @@ class LocationsController < ApplicationController
   def create
     location = Location.new(strong_params)
     status = location.save
-    render json: {saved: status}
+    render json: { saved: status }
+  end
+
+  def update
+    location = Location.find(params[:id])
+    location.update(params_to_attributes)
+    render json: location
   end
 
   private
@@ -30,13 +38,85 @@ class LocationsController < ApplicationController
             items_attributes: [
               :name,
               :price,
-              options_attributes: [
-                :name,
-                :price
+              options_attributes: %i[
+                name
+                price
               ]
             ]
           ]
         ]
       )
+  end
+
+  def stronger_params
+    params
+      .require(:location)
+      .permit :name,
+              :description,
+              :id,
+              :menuId,
+              categories: %i[id cuid name description menu_id],
+              items: %i[id cuid name description price category_id],
+              options: %i[id cuid name price item_id]
+  end
+
+  def params_to_attributes
+    categories = stronger_params[:categories]
+    items = stronger_params[:items]
+    options = stronger_params[:options]
+    location = stronger_params
+
+    {
+      id: location[:id],
+      name: location[:name],
+      description: location[:description],
+      menus_attributes: [{
+        id: location[:menuId],
+        categories_attributes: categories_attributes(categories, items, options)
+      }]
+    }.compact
+  end
+
+  def categories_attributes(categories, items, options)
+    categories.map do |category|
+      c_items = items.select do |item|
+        item[:categoryCuid] == category[:cuid] || item[:category_id] == category[:id]
+      end
+
+      {
+        id: category[:id],
+        name: category[:name],
+        menu_id: category[:menu_id],
+        items_attributes: items_attributes(c_items, options)
+      }.compact
+    end
+  end
+
+  def items_attributes(items, options)
+    items.map do |item|
+      i_options = options.select do |option|
+        option[:itemCuid] == item[:cuid] || option[:item_id] == item[:id]
+      end
+
+      {
+        id: item[:id],
+        name: item[:name],
+        description: item[:description],
+        price: item[:price],
+        category_id: item[:category_id],
+        options_attributes: options_attributes(i_options)
+      }.compact
+    end
+  end
+
+  def options_attributes(options)
+    options.map do |option|
+      {
+        id: option[:id],
+        name: option[:name],
+        price: option[:price],
+        item_id: option[:item_id]
+      }.compact
+    end
   end
 end
