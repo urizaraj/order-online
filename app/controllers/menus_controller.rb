631c736d5409cd49cf25c39b42461b9c99047842
Alexander Urizar <urizaraj@gmail.com>
2018-04-28 13:20:08 -0400

    Formatting

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class MenusController < ApplicationController
   def show
     menu = Menu.find(params[:id])
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,21 +1,23 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def sign_in
     # user = User.find_by(name: params[:name])
-    user = User.where('lower(name) = ?', params[:name].downcase).first 
+    user = User.where('lower(name) = ?', params[:name].downcase).first
 
-    valid = user && user.authenticate(params[:password])
-    return render json: {message: 'invalid'} unless valid
+    valid = user&.authenticate(params[:password])
+    return render json: { message: 'invalid' } unless valid
 
-    payload = {user: user.id}
+    payload = { user: user.id }
     token = JWT.encode payload, nil, 'none'
-    render json: {token: token, id: user.id, name: user.name}
+    render json: { token: token, id: user.id, name: user.name }
   end
 
   def sign_in_token
     payload = JWT.decode params[:token], nil, false
     user = User.find(payload[0]['user'])
 
-    return render json: {message: 'invalid'} unless user
+    return render json: { message: 'invalid' } unless user
 
     render json: user
   end
