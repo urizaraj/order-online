5afc27c3f777bafb819f69462040fc591e61b46a
Alexander Urizar <urizaraj@gmail.com>
2018-04-30 16:15:49 -0400

    Break CurrentOrder into different components

--- src/components/orders/CurrentOrder.js
+++ src/components/orders/CurrentOrder.js
@@ -8,51 +8,6 @@ import Icon from '@fortawesome/react-fontawesome'
 
 import { DFlex } from '../elements'
 
-let OldCurrentOrder = props => {
-  let total = 0
-
-  const itemList = props.orderItems.map(item => {
-    let itemPrice = item.price
-    item.selectedOptions.forEach(({ price }) => (itemPrice += price))
-    total += itemPrice
-
-    return (
-      <div key={item.cuid} className='d-flex'  >
-        <div className='p-2' >
-          <button onClick={() => props.removeOrderItem(item.cuid)} className='btn btn-link' ><Icon icon='times' /></button> {item.name}
-        </div>
-        <div className='ml-auto align-self-center p-2' >
-          ${itemPrice}
-        </div>
-      </div>
-    )
-  })
-
-  return (
-    <div>
-      <h4>Current Order</h4>
-      {itemList}
-      <DFlex opt='mb-2' >
-        <h4 className='align-self-center mb-0' >Total</h4>
-        <div className='ml-auto p-2' >
-          ${total}
-        </div>
-      </DFlex>
-
-      <DFlex>
-        <button className='btn btn-success ml-auto' onClick={() => props.saveOrder()} >
-          <Icon icon='check' /> Save Order
-        </button>
-      </DFlex>
-    </div>
-  )
-}
-
-
-
-
-
-
 class CurrentOrder extends React.Component {
   render() {
     return (
