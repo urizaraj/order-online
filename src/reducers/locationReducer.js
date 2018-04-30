db1de0d8c37fff7b17b122fff5cce15b51fb6792
Alexander Urizar <urizaraj@gmail.com>
2018-04-30 13:57:44 -0400

    Update locationReducer to properly update state

--- src/reducers/locationReducer.js
+++ src/reducers/locationReducer.js
@@ -1,16 +1,16 @@
 const locationReducer = (state = { locations: [], loading: false, location: null }, action) => {
   switch (action.type) {
     case 'FETCH_LOCATIONS':
-      return { loading: false, locations: action.locations }
+      return { ...state, loading: false, locations: action.locations }
 
     case 'FETCH_LOCATION':
-      return { loading: false, locations: [], location: action.location }
+      return { ...state, loading: false, location: action.location }
 
     case 'LOADING_LOCATIONS':
-      return { loading: true, locations: [] }
+      return { ...state, loading: true, locations: [] }
 
     case 'LOADING_LOCATION':
-      return { loading: true, location: null }
+      return { ...state, loading: true, location: null }
 
     default:
       return state
