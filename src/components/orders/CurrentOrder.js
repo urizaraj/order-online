33d4c867491948a0619cb7d8fc73871291cbfc4b
Alexander Urizar <urizaraj@gmail.com>
2018-04-30 16:15:08 -0400

    Break CurrentOrder into different components

--- src/components/orders/CurrentOrder.js
+++ src/components/orders/CurrentOrder.js
@@ -8,7 +8,7 @@ import Icon from '@fortawesome/react-fontawesome'
 
 import { DFlex } from '../elements'
 
-let CurrentOrder = props => {
+let OldCurrentOrder = props => {
   let total = 0
 
   const itemList = props.orderItems.map(item => {
@@ -45,6 +45,64 @@ let CurrentOrder = props => {
         </button>
       </DFlex>
     </div>
+  )
+}
+
+
+
+
+
+
+class CurrentOrder extends React.Component {
+  render() {
+    return (
+      <div>
+        <h4>Current Order</h4>
+        <OrderItemList orderItems={this.props.orderItems} removeOrderItem={this.props.removeOrderItem} />
+
+        <DFlex opt='mb-2' >
+          <h4 className='align-self-center mb-0' >Total</h4>
+          <div className='ml-auto p-2' >
+            ${this.total()}
+          </div>
+        </DFlex>
+
+        <DFlex>
+          <button className='btn btn-success ml-auto' onClick={this.saveOrder} >
+            <Icon icon='check' /> Save Order
+          </button>
+        </DFlex>
+      </div>
+    )
+  }
+
+  saveOrder = event => {
+    this.props.saveOrder()
+  }
+
+  total = () => {
+    return this.props.orderItems.reduce((total, item) => total + item.price, 0)
+  }
+}
+
+const OrderItemList = props => {
+  return props.orderItems.map(oi => <OrderItem {...oi} key={oi.cuid} removeOrderItem={props.removeOrderItem} />)
+}
+
+const OrderItem = props => {
+  const itemPrice = props.selectedOptions.reduce((total, option) => {
+    return total + option.price
+  }, props.price)
+
+  return (
+    <DFlex>
+      <div className='p-2' >
+        <button onClick={() => props.removeOrderItem(props.cuid)} className='btn btn-link' ><Icon icon='times' /></button> {props.name}
+      </div>
+      <div className='ml-auto align-self-center p-2' >
+        ${itemPrice}
+      </div>
+    </DFlex>
   )
 }
 
