8d3c12057b710e82605daff604d2202936b705ad
Alexander Urizar <urizaraj@gmail.com>
2018-05-01 13:48:01 -0400

    Add order model validation to check if order has at least one order item

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,6 +4,7 @@ class Order < ApplicationRecord
   has_many :order_items
   
   accepts_nested_attributes_for :order_items
+  validate :at_least_one_order_item
 
   def user_name
     user.name
@@ -11,5 +12,10 @@ class Order < ApplicationRecord
 
   def location_name
     location.name
+  end
+
+  def at_least_one_order_item
+    return if order_items.length > 0
+    errors.add :base, 'Must have at least one order item.'
   end
 end
