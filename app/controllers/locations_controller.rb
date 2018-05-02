da6b6e02ca789381af46783475aad20dfd9c144c
Alexander Urizar <urizaraj@gmail.com>
2018-05-02 18:06:29 -0400

    Refactor locationscontroller to be less verbose

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class LocationsController < ApplicationController
+  before_action :authenticate, only: %i[create update]
+
   def index
     page = params[:page].to_i || 1
     offset = (page - 1) * 4
@@ -18,7 +20,6 @@ class LocationsController < ApplicationController
   def create
     location = Location.new(params_to_attributes)
     status = location.save
-
     render json: { saved: status }
   end
 
@@ -33,28 +34,6 @@ class LocationsController < ApplicationController
   def strong_params
     params
       .require(:location)
-      .permit(
-        :name,
-        :description,
-        menus_attributes: [
-          categories_attributes: [
-            :name,
-            items_attributes: [
-              :name,
-              :price,
-              options_attributes: %i[
-                name
-                price
-              ]
-            ]
-          ]
-        ]
-      )
-  end
-
-  def stronger_params
-    params
-      .require(:location)
       .permit :name,
               :description,
               :id,
@@ -65,10 +44,9 @@ class LocationsController < ApplicationController
   end
 
   def params_to_attributes
-    categories = stronger_params[:categories]
-    items = stronger_params[:items]
-    options = stronger_params[:options]
-    location = stronger_params
+    location = strong_params
+
+    categories, items, options = location.values_at(:categories, :items, :options)
 
     {
       id: location[:id],
@@ -82,53 +60,35 @@ class LocationsController < ApplicationController
   end
 
   def categories_attributes(categories, items, options)
+    keys = %w[id name menu_id]
     categories.map do |category|
-      c_items = items.select do |item|
-        if item[:category_id]
-          item[:category_id] == category[:id]
-        else
-          item[:categoryCuid] == category[:cuid]
-        end
-      end
-
-      {
-        id: category[:id],
-        name: category[:name],
-        menu_id: category[:menu_id],
-        items_attributes: items_attributes(c_items, options)
-      }.compact
+      c_items = matcher(category, items, :category_id, :categoryCuid)
+      h = category.select { |key, _v| keys.include?(key) }
+      h.merge(items_attributes: items_attributes(c_items, options))
     end
   end
 
   def items_attributes(items, options)
+    keys = %w[id name description price category_id]
     items.map do |item|
-      i_options = options.select do |option|
-        if option[:item_id]
-          option[:item_id] == item[:id]
-        else
-          option[:itemCuid] == item[:cuid]
-        end
-      end
-
-      {
-        id: item[:id],
-        name: item[:name],
-        description: item[:description],
-        price: item[:price],
-        category_id: item[:category_id],
-        options_attributes: options_attributes(i_options)
-      }.compact
+      i_options = matcher(item, options, :item_id, :itemCuid)
+      h = item.select { |key, _v| keys.include?(key) }
+      h.merge(options_attributes: options_attributes(i_options))
     end
   end
 
   def options_attributes(options)
-    options.map do |option|
-      {
-        id: option[:id],
-        name: option[:name],
-        price: option[:price],
-        item_id: option[:item_id]
-      }.compact
+    keys = %w[id name price item_id]
+    options.map { |option| option.select { |key, _v| keys.include?(key) } }
+  end
+
+  def matcher(parent, children, id, cuid)
+    children.select do |child|
+      if child[id]
+        child[id] == parent[:id]
+      else
+        child[cuid] == parent[:cuid]
+      end
     end
   end
 end
