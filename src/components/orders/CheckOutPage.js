612054fdc35fe1579c98a4f1d5ea981a688e24a4
Alexander Urizar <urizaraj@gmail.com>
2018-05-04 12:45:02 -0400

    Add payment type fields and card number field

--- src/components/orders/CheckOutPage.js
+++ src/components/orders/CheckOutPage.js
@@ -13,14 +13,29 @@ import Icon from '@fortawesome/react-fontawesome'
 
 class CheckOutPage extends Component {
   render() {
+    const cash = this.props.paymentType === 'cash'
+    const card = this.props.paymentType === 'card'
     return (
       <div>
         <Row>
           <BCol>
+
+            <DFlex opt='justify-content-around' >
+              <Radio name='paymentType' value='cash' checked={cash} onChange={this.onChange} >
+                <span className='font-weight-light h2'><Icon icon={cash ? 'check-square' : 'square'} /> Cash</span>
+              </Radio>
+              <Radio name='paymentType' value='card' checked={card} onChange={this.onChange} >
+                <span className='font-weight-light h2' ><Icon icon={card ? 'check-square' : 'square'} /> Card</span>
+              </Radio>
+            </DFlex>
+
             <FormGroup>
-              <Btn opt='primary' onClick={this.goBack} >
-                <Icon icon='arrow-left' /> Go Back
-              </Btn>
+              <FormControl
+                disabled={cash}
+                placeholder='Card Number'
+                name='cardNumber'
+                value={this.props.cardNumber}
+                onChange={this.onChange} />
             </FormGroup>
 
             <FormGroup>
@@ -34,11 +49,11 @@ class CheckOutPage extends Component {
 
             <FormGroup>
               <FormControl
-                placeholder='Full Name'
-                name='fullName'
-                value={this.props.fullName}
+                placeholder='Street'
+                name='street'
+                value={this.props.street}
                 onChange={this.onChange} />
-                
+
             </FormGroup>
           </BCol>
 
@@ -60,6 +75,7 @@ class CheckOutPage extends Component {
         </Row>
 
         <div className='text-right'>
+          <Btn opt='primary' onClick={this.goBack} > <Icon icon='arrow-left' /> Go Back </Btn>
           <Btn opt='success' onClick={this.saveOrder} ><Icon icon='check' /> Save Order</Btn>
         </div>
       </div>
