a22369af2436f148583a9f2a80bf41727b540e43
Alexander Urizar <urizaraj@gmail.com>
2018-05-09 17:54:43 -0400

    OrderSerializer renders more order attributes

--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,9 @@
+# frozen_string_literal: true
+
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :user_name, :location_name, :created_at
+  attributes :id, :user_name, :location_name, :created_at,
+             :payment_type, :full_name, :address, :city, :state, :zipcode,
+             :delivery_type
 
   belongs_to :location
   belongs_to :user
